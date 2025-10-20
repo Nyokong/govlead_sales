26da1053deaf49470fe476c552a65c68895b5cbd
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-10-20 23:39:20 +0200

    see some chnages

--- components/reuseables/logout/logout.tsx
+++ components/reuseables/logout/logout.tsx
@@ -5,10 +5,6 @@ import { signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import { redirect } from "next/navigation";
 
-// import { db } from "@/db";
-// import { users } from "@/db/schema";
-// import { eq } from "drizzle-orm";
-
 type SignOutButtonProps = {
   email: string;
   children: React.ReactNode;
