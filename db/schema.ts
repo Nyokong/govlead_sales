5c865b803d621a92e909550bbe70eb18c0cb9137
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-10-21 12:34:30 +0200

    multiple primary keys

--- db/schema.ts
+++ db/schema.ts
@@ -64,7 +64,7 @@ export const users = pgTable("users", {
 
 export const sessions = pgTable("session", {
   sessionToken: text("sessionToken").primaryKey(),
-  userId: text("uID")
+  userId: text("userID")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   expires: timestamp("expires", { mode: "date" }).notNull(),
