d18060028cc3ce60168e348ddcdd48bf3671ce10
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-10-21 16:29:38 +0200

    ui changes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,6 +8,7 @@ import Loading from "../loading";
 import { Ring } from "ldrs/react";
 import { Label } from "@/components/ui/label";
 import { redirect } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
@@ -30,20 +31,30 @@ export default function Dashboard() {
 
   if (session?.user) {
     return (
-      <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-        <div className="p-3.5 flex flex-row gap-[20px] bg-white rounded-2xl shadow-sm inset-shadow-xs px-[40px]">
-          <Label className="text-[#ffffff] py-2 px-4 bg-[#2929298f] rounded-2xl">
-            Email
-          </Label>{" "}
-          <p>{session?.user.email}</p>
+      <div className="font-sans flex flex-col md:flex-row items-center justify-center min-h-[20vh] p-3 pb-20 gap-2 sm:p-10">
+        <div className="flex flex-col gap-2 bg-[#ffffff] p-2 rounded-2xl md:w-[400px] md:min-h-[400px]">
+          <div className="p-3.5 flex flex-row gap-[20px] bg-white rounded-2xl shadow-sm inset-shadow-xs px-[40px]">
+            <Label className="text-[#ffffff] py-2 px-4 bg-[#2929298f] rounded-2xl">
+              Email
+            </Label>{" "}
+            <p>{session?.user.email}</p>
+          </div>
+          <Separator />
+          <div className="p-3.5 flex flex-row gap-[20px] bg-white rounded-2xl shadow-sm inset-shadow-xs px-[40px]">
+            <Label>Some cool thing will be here</Label>
+          </div>
         </div>
-        <div>
+        <div className="flex flex-col gap-2 bg-[#ffffff] p-2 rounded-2xl md:w-[400px] md:min-h-[400px]">
+          <div className="p-3.5 flex flex-row gap-[20px] bg-white rounded-2xl shadow-sm inset-shadow-xs px-[40px]">
+            Dont press this yet
+          </div>
+          <Separator />
           <SignOutButton>
             <Button
               onClick={() => {
                 setLoading(true);
               }}
-              className="w-[120px] rounded-3xl flex justify-center items-center h-[40px] cursor-pointer"
+              className="w-[100%] rounded-3xl flex justify-center items-center h-[40px] cursor-pointer bg-[#f4f4f4] text-black inset-shadow-xs"
             >
               {loading == true ? (
                 <Ring size={20} speed={1.5} bgOpacity={0.25} color="white" />
