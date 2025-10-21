13d62f4c79f3baef299b18cfe43acf88aa5345db
Callmekay <50559078+Nyokong@users.noreply.github.com>
2025-10-21 12:32:02 +0200

    checking if session only can work

--- db/schema.ts
+++ db/schema.ts
@@ -64,7 +64,7 @@ export const users = pgTable("users", {
 
 export const sessions = pgTable("session", {
   sessionToken: text("sessionToken").primaryKey(),
-  userId: text("userID")
+  userId: text("uID")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   expires: timestamp("expires", { mode: "date" }).notNull(),
